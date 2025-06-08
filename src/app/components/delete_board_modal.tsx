485b5906685f001145b5c6bf2fdc7a0fa3fec056
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2025-06-08 08:25:43 +0100

    Enhance Deleteboardmodal and Header components; integrate deleteBoard functionality and improve board selection logic

--- src/app/components/delete_board_modal.tsx
+++ src/app/components/delete_board_modal.tsx
@@ -1,32 +1,61 @@
 import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../redux/store";
-import { toggleDeleteModal } from "../redux/slices/slices";
+import {
+  toggleDeleteModal,
+  deleteBoard,
+
+} from "../redux/slices/slices";
+import { useEffect } from "react";
 
 const Deleteboardmodal = () => {
-  const targetBoard = useSelector(
+  const currentBoard = useSelector(
     (state: RootState) => state.currentBoard.value
   );
+  const boards = useSelector((state: RootState) => state.boards.value);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+
+    localStorage.setItem("boards", JSON.stringify(boards));
+  }, [boards, dispatch]);
+
   return (
-    <div className="absolute flex items-center justify-center z-[100] h-screen w-screen bg-[#00000052]"
-    onClick={()=>{
-        dispatch(toggleDeleteModal())
-    }}
+    <div
+      className="absolute flex items-center justify-center z-[100] h-screen w-screen bg-[#00000052]"
+      onClick={() => {
+        dispatch(toggleDeleteModal());
+      }}
     >
-      <div className="bg-white p-10 rounded-xl flex flex-col w-[520px] space-y-4">
+      <div
+        className="bg-white p-10 rounded-xl flex flex-col w-[520px] space-y-4"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
         <h2 className="font-semibold text-xl text-red-500">
           Delete this board?
         </h2>
         <p className="text-[#7C8CA4]">
           Are you sure you want to delete the &quot;
-          {targetBoard && targetBoard.name}&quot; board? This action will remove
-          all columns and tasks and cannot be reversed.
+          {currentBoard && currentBoard.name}&quot; board? This action will
+          remove all columns and tasks and cannot be reversed.
         </p>
         <div className="flex space-x-8 justify-between text-lg font-bold">
-          <button className="w-1/2 border bg-red-500 text-white cursor-pointer p-3 py-2 rounded-full">
+          <button
+            className="w-1/2 border bg-red-500 text-white cursor-pointer p-3 py-2 rounded-full"
+            onClick={() => {
+              dispatch(deleteBoard(currentBoard && currentBoard.id));
+              dispatch(toggleDeleteModal());
+            }}
+          >
             Delete
           </button>
-          <button className="w-1/2 border bg-[#b5c0d2] cursor-pointer border-none p-3 py-2 rounded-full">
+          <button
+            className="w-1/2 border bg-[#b5c0d2] cursor-pointer border-none p-3 py-2 rounded-full"
+            onClick={() => {
+              dispatch(toggleDeleteModal());
+            }}
+          >
             Cancel
           </button>
         </div>
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PiKanbanFill } from "react-icons/pi";
 import { FaPlus } from "react-icons/fa";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../redux/store";
-import { toggleAddTaskModal,toggleEditBoardModal,toggleDeleteModal } from "../redux/slices/slices";
+import {
+  toggleAddTaskModal,
+  toggleEditBoardModal,
+  toggleDeleteModal,
+  setCurrentBoard,
+} from "../redux/slices/slices";
 
 const Header = () => {
   const [isOptions, setIsOptions] = useState(false);
@@ -12,7 +17,18 @@ const Header = () => {
   const currentBoard = useSelector(
     (state: RootState) => state.currentBoard.value
   );
+  const boards = useSelector((state: RootState) => state.boards.value);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (boards.length > 0) {
+      dispatch(setCurrentBoard(boards[0]));
+    } else {
+      dispatch(setCurrentBoard(null));
+      localStorage.removeItem("currentBoard");
+    }
+  }, [dispatch, boards]);
+
   return (
     <header
       className={`flex p-5 relative font-semibold ${
@@ -27,47 +43,49 @@ const Header = () => {
         <h1 className="text-[22px] capitalize">
           {currentBoard ? currentBoard.name : "No Board Selected"}
         </h1>
-        <div className="flex gap-3">
-          <button
-            className="flex items-center gap-1 text-white p-4 py-2.5 cursor-pointer rounded-3xl bg-[#7247ce]"
-            onClick={() => {
-              dispatch(toggleAddTaskModal());
-            }}
-          >
-            <FaPlus />
-            Add New Task
-          </button>
-          <button
-            className="text-xl cursor-pointer"
-            onClick={() => {
-              setIsOptions((isOptions) => !isOptions);
-            }}
-          >
-            <BsThreeDotsVertical />
-          </button>
-          {isOptions && (
-            <div className="bg-white text-md z-[100] p-4 rounded-md flex flex-col items-start gap-1 shadow-[0px_1px_2px_#7C8CA4] absolute top-full right-5">
-              <button
-                className="font-normal text-[#7C8CA4] cursor-pointer "
-                onClick={() => {
-                  dispatch(toggleEditBoardModal());
-                  setIsOptions((isOptions) => !isOptions);
-                }}
-              >
-                Edit board
-              </button>
-              <button
-                className="font-normal cursor-pointer text-red-500 "
-                onClick={() => {
-                  setIsOptions((isOptions) => !isOptions);
-                  dispatch(toggleDeleteModal())
-                }}
-              >
-                Delete board
-              </button>
-            </div>
-          )}
-        </div>
+        {currentBoard && (
+          <div className="flex gap-3">
+            <button
+              className="flex items-center gap-1 text-white p-4 py-2.5 cursor-pointer rounded-3xl bg-[#7247ce]"
+              onClick={() => {
+                dispatch(toggleAddTaskModal());
+              }}
+            >
+              <FaPlus />
+              Add New Task
+            </button>
+            <button
+              className="text-xl cursor-pointer"
+              onClick={() => {
+                setIsOptions((isOptions) => !isOptions);
+              }}
+            >
+              <BsThreeDotsVertical />
+            </button>
+            {isOptions && (
+              <div className="bg-white text-md z-[100] p-4 rounded-md flex flex-col items-start gap-1 shadow-[0px_1px_2px_#7C8CA4] absolute top-full right-5">
+                <button
+                  className="font-normal text-[#7C8CA4] cursor-pointer "
+                  onClick={() => {
+                    dispatch(toggleEditBoardModal());
+                    setIsOptions((isOptions) => !isOptions);
+                  }}
+                >
+                  Edit board
+                </button>
+                <button
+                  className="font-normal cursor-pointer text-red-500 "
+                  onClick={() => {
+                    setIsOptions((isOptions) => !isOptions);
+                    dispatch(toggleDeleteModal());
+                  }}
+                >
+                  Delete board
+                </button>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </header>
   );

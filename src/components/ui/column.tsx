28ec9c6e9b0967b55d284de1765f4aead43183d1
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2025-10-04 12:17:49 +0100

    feat(ui): Add drag-and-drop visual feedback
    This commit enhances the user experience of the drag-and-drop feature by providing visual feedback when an item is being dragged over a column.
    
    - A new `isDragging` state has been added to the `ColumnComponent` to track whether an item is being dragged over the column.
    - The background color of the column now changes when an item is being dragged over it, providing a clear visual cue to the user.

--- src/components/ui/column.tsx
+++ src/components/ui/column.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Column, Task } from "@/types";
 
 import TaskCard from "./task-card";
@@ -13,17 +13,39 @@ type Props = {
   setViewTaskOpen: (open: boolean) => void;
 };
 
-const ColumnComponent = ({ column, handleDrop, setDragOriginColumnId, setDraggedItem, tasks, setViewTaskOpen }: Props) => {
+const ColumnComponent = ({
+  column,
+  handleDrop,
+  setDragOriginColumnId,
+  setDraggedItem,
+  tasks,
+  setViewTaskOpen,
+}: Props) => {
+  const [isDragging, setIsDragging] = useState(false);
   return (
     <div
-      className="w-[280px] max-md:w-[190px]"
+      className="w-[280px] max-md:w-[190px] p-2"
       onDragOver={(e) => {
         e.preventDefault();
+      }}
+      onDragEnter={() => {
+        setIsDragging(true);
+      }}
+      onDragLeave={() => {
+        setIsDragging(false);
+      }}
+      onDragEnd={() => {
+        setIsDragging(false);
+      }}
+      style={{
+        backgroundColor: isDragging
+          ? "rgba(200, 200, 200, 0.2)"
+          : "transparent",
+        transition: "background-color 0.3s ease",
       }}
       onDrop={() => {
         handleDrop(column.id);
       }}
-
     >
       <h1 className="font-semibold  text-md capitalize text-[#7C8CA4] tracking-widest flex gap-1 pb-3">
         {column && column.name}
@@ -33,7 +55,14 @@ const ColumnComponent = ({ column, handleDrop, setDragOriginColumnId, setDragged
         {tasks?.map((task: Task, index: number) => {
           if (!task) return null;
           return (
-            <TaskCard key={index} task={task} columnId={column.id} setViewTaskOpen={setViewTaskOpen} setDragOriginColumnId={setDragOriginColumnId} setDraggedItem={setDraggedItem} />
+            <TaskCard
+              key={index}
+              task={task}
+              columnId={column.id}
+              setViewTaskOpen={setViewTaskOpen}
+              setDragOriginColumnId={setDragOriginColumnId}
+              setDraggedItem={setDraggedItem}
+            />
           );
         })}
       </div>
